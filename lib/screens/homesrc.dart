5c83c7e5283058e00460babd571d529945d9cb50
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-03-13 02:39:20 +0530

    Shivam Sir

--- lib/screens/homesrc.dart
+++ lib/screens/homesrc.dart
@@ -21,204 +21,209 @@ class _OfficeHomeState extends State<OfficeHome> {
   String? currentscr = "Home";
   @override
   void initState() {
+    try{
     FirebaseFirestore.instance
         .collection("users")
         .doc(user!.uid)
         .get()
         .then((value) {
       this.loggedInUser = UserModel.fromMap(value.data());
-      setState(() {});
     });
+    print(user!.uid);
+    print(loggedInUser.email);
+    }catch(e){
+      print(e);
+    }
   }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 60,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(bottomRight: Radius.circular(5)),
-              color: white,
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 5),
-                  blurRadius: 10,
-                  color: secondbg.withOpacity(0.20),
-                ),
-              ],
-            ),
-            child: Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    left: 30,
+    
+      return Scaffold(
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(5)),
+                color: white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 5),
+                    blurRadius: 10,
+                    color: secondbg.withOpacity(0.20),
                   ),
-                  child: Center(
-                    child: Image(
-                      image: AssetImage("assets/icons/imaicon.png"),
-                      height: 45,
-                      filterQuality: FilterQuality.high,
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 30,
                     ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Container(
-                  height: double.infinity,
-                  width: 280,
-                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  child: Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: white,
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 5),
-                            blurRadius: 20,
-                            color: secondbg.withOpacity(0.20),
-                          ),
-                        ],
+                    child: Center(
+                      child: Image(
+                        image: AssetImage("assets/icons/imaicon.png"),
+                        height: 45,
+                        filterQuality: FilterQuality.high,
                       ),
-                      child: Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        child: SingleChildScrollView(
-                          physics: BouncingScrollPhysics(),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                height: 30,
-                              ),
-                              InkWell(
-                                onTap: () => setState(() {
-                                  currentscr = 'Home';
-                                }),
-                                child: Container(
-                                  height: 50,
-                                  width: double.infinity,
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(30),
-                                      color: cheryred),
-                                  child: Text(
-                                    "Home",
-                                    style: TextStyle(
-                                      fontFamily: "Nunito",
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Container(
+                    height: double.infinity,
+                    width: 280,
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    child: Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: white,
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(0, 5),
+                              blurRadius: 20,
+                              color: secondbg.withOpacity(0.20),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 10),
+                          child: SingleChildScrollView(
+                            physics: BouncingScrollPhysics(),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  height: 30,
+                                ),
+                                InkWell(
+                                  onTap: () => setState(() {
+                                    currentscr = 'Home';
+                                  }),
+                                  child: Container(
+                                    height: 50,
+                                    width: double.infinity,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(30),
+                                        color: cheryred),
+                                    child: Text(
+                                      "Home",
+                                      style: TextStyle(
+                                        fontFamily: "Nunito",
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                        color: white,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Text(
-                                "Progam Management",
-                                style: TextStyle(
-                                  fontFamily: "Nunito",
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xffD3D3D3),
+                                SizedBox(
+                                  height: 20,
                                 ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              InkWell(
-                                onTap: () => setState(() {
-                                  currentscr = 'View';
-                                }),
-                                child: Container(
-                                  height: 50,
-                                  width: double.infinity,
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(30),
-                                      color: cheryred),
-                                  child: Text(
-                                    "View All",
-                                    style: TextStyle(
-                                      fontFamily: "Nunito",
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: white,
+                                Text(
+                                  "Progam Management",
+                                  style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xffD3D3D3),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                InkWell(
+                                  onTap: () => setState(() {
+                                    currentscr = 'View';
+                                  }),
+                                  child: Container(
+                                    height: 50,
+                                    width: double.infinity,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(30),
+                                        color: cheryred),
+                                    child: Text(
+                                      "View All",
+                                      style: TextStyle(
+                                        fontFamily: "Nunito",
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                        color: white,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              InkWell(
-                                onTap: () => setState(() {
-                                  currentscr = 'PgmReg';
-                                }),
-                                child: Container(
-                                  height: 50,
-                                  width: double.infinity,
-                                  alignment: Alignment.center,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(30),
-                                      color: cheryred),
-                                  child: Text(
-                                    "Register Program",
-                                    style: TextStyle(
-                                      fontFamily: "Nunito",
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: white,
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                InkWell(
+                                  onTap: () => setState(() {
+                                    currentscr = 'PgmReg';
+                                  }),
+                                  child: Container(
+                                    height: 50,
+                                    width: double.infinity,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(30),
+                                        color: cheryred),
+                                    child: Text(
+                                      "Register Program",
+                                      style: TextStyle(
+                                        fontFamily: "Nunito",
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                        color: white,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.only(right: 10, top: 10, bottom: 5),
+                  Expanded(
                     child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: white,
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 5),
-                            blurRadius: 20,
-                            color: secondbg.withOpacity(0.20),
-                          ),
-                        ],
-                      ),
-                      // child: Pgmreg(uid: user!.uid),
-                      child: Widgetwrapper(
-                        src: currentscr,
-                        uid: user!.uid,
+                      padding: EdgeInsets.only(right: 10, top: 10, bottom: 5),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: white,
+                          boxShadow: [
+                            BoxShadow(
+                              offset: Offset(0, 5),
+                              blurRadius: 20,
+                              color: secondbg.withOpacity(0.20),
+                            ),
+                          ],
+                        ),
+                        // child: Pgmreg(uid: user!.uid),
+                        child: Widgetwrapper(
+                          src: currentscr,
+                          uid: user!.uid,
+                        ),
                       ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
+          ],
+        ),
+      );}
 }
 
 class Widgetwrapper extends StatelessWidget {
