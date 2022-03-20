6067bb64469e580dfaad5ef33b397596dcc3fe56
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-03-20 23:59:15 +0530

    Update

--- lib/componets/vscroll.dart
+++ lib/componets/vscroll.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 
+
 class Vscroll extends MaterialScrollBehavior {
   @override
   Set<PointerDeviceKind> get dragDevices => {
