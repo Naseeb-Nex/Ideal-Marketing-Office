726c492b98adc64948904d83249ddddcbca9dca5
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-03-21 21:33:10 +0530

    Tech card elapsing

--- lib/widgets/home_widget.dart
+++ lib/widgets/home_widget.dart
@@ -1,5 +1,7 @@
+import 'dart:html';
 import 'dart:math';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:test2/constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -8,6 +10,7 @@ import 'package:test2/screens/homesrc.dart';
 
 import 'package:intl/intl.dart';
 import 'package:flutter/gestures.dart';
+import 'package:test2/componets/vscroll.dart';
 
 class Homewidget extends StatefulWidget {
   String? uid;
@@ -23,7 +26,9 @@ class _HomewidgetState extends State<Homewidget> {
   @override
   void initState() {
     super.initState();
-    pgmsetup();
+    if (mounted) {
+      pgmsetup();
+    }
   }
 
   final List techprofile = [];
@@ -33,180 +38,169 @@ class _HomewidgetState extends State<Homewidget> {
     return Container(
       height: double.infinity,
       width: double.infinity,
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 30,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Home",
+            style: TextStyle(
+              fontFamily: "Nunito",
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Color(0xff8F9194),
             ),
-            Text(
-              "Home",
-              style: TextStyle(
-                fontFamily: "Nunito",
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+          ),
+          Container(
+            height: 5,
+            width: 36,
+            decoration: BoxDecoration(
                 color: Color(0xff8F9194),
-              ),
-            ),
-            Container(
-              height: 5,
-              width: 36,
-              decoration: BoxDecoration(
-                  color: Color(0xff8F9194),
-                  borderRadius: BorderRadius.circular(10)),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                height: 100,
-                width: double.infinity,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Flexible(
-                        flex: 3,
-                        child: Container(
-                          height: double.infinity,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    height: 10,
-                                    width: 10,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        color: cheryred),
-                                  ),
-                                  Text(
-                                    " Pending Programs",
-                                    style: TextStyle(
-                                      fontFamily: "Nunito",
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xff273746),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                "$p",
-                                style: TextStyle(
-                                  fontFamily: "Nunito",
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xff273746),
+                borderRadius: BorderRadius.circular(10)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 100,
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Flexible(
+                      flex: 3,
+                      child: Container(
+                        height: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 10,
+                                  width: 10,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: cheryred),
                                 ),
-                              ),
-                            ],
-                          ),
-                        )),
-                    Flexible(
-                        flex: 3,
-                        child: Container(
-                          height: double.infinity,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    height: 10,
-                                    width: 10,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        color: Colors.greenAccent),
+                                Text(
+                                  " Pending Programs",
+                                  style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xff273746),
                                   ),
-                                  Text(
-                                    " Completed Programs",
-                                    style: TextStyle(
-                                      fontFamily: "Nunito",
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xff273746),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                "$c",
-                                style: TextStyle(
-                                  fontFamily: "Nunito",
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xff273746),
                                 ),
+                              ],
+                            ),
+                            Text(
+                              "$p",
+                              style: TextStyle(
+                                fontFamily: "Nunito",
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff273746),
                               ),
-                            ],
-                          ),
-                        )),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 50,
-                      ),
-                      Icon(
-                        Icons.person,
-                        color: Colors.black26,
-                      ),
-                      Text(
-                        "  Technicians",
-                        style: TextStyle(
-                          fontFamily: "Nunito",
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black26,
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 100,
-                      ),
-                      Container(
-                        height: 2,
-                        width: 100,
-                        decoration: BoxDecoration(
-                            color: Colors.black26,
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                    ],
-                  ),
+                      )),
+                  Flexible(
+                      flex: 3,
+                      child: Container(
+                        height: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 10,
+                                  width: 10,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.greenAccent),
+                                ),
+                                Text(
+                                  " Completed Programs",
+                                  style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xff273746),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "$c",
+                              style: TextStyle(
+                                fontFamily: "Nunito",
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff273746),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
                 ],
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                Example(
-                  userid: widget.uid,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 50,
+              ),
+              Icon(
+                Icons.person,
+                color: Colors.black26,
+              ),
+              Text(
+                "  Technicians",
+                style: TextStyle(
+                  fontFamily: "Nunito",
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black26,
                 ),
-              ],
-            )
-          ],
-        ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 100,
+              ),
+              Container(
+                height: 2,
+                width: 100,
+                decoration: BoxDecoration(
+                    color: Colors.black26,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: Example(
+              userid: widget.uid,
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -272,22 +266,19 @@ class Example extends StatelessWidget {
             print(a);
             a['uid'] = document.id;
           }).toList();
-          return Container(
-            child: ListView(
+          return ScrollConfiguration(
+            behavior: Vscroll(),
+            child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              children: [
-                SizedBox(
-                  width: 30,
-                ),
-                for (var i = 0; i < techprofile.length; i++) ...[
-                  Techcard(
-                    name: techprofile[i]['name'],
-                    img: techprofile[i]['pic'],
-                    username: techprofile[i]['username'],
-                    uid: techprofile[i]['uid'],
-                  )
-                ]
-              ],
+              itemCount: techprofile.length,
+              itemBuilder: (context, i) {
+                return Techcard(
+                  name: techprofile[i]['name'],
+                  img: techprofile[i]['pic'],
+                  username: techprofile[i]['username'],
+                  uid: techprofile[i]['uid'],
+                );
+              },
             ),
           );
         });
@@ -314,7 +305,7 @@ class _TechcardState extends State<Techcard> {
   @override
   void initState() {
     super.initState();
-    startup();
+    if (mounted) startup();
   }
 
   @override
@@ -356,38 +347,35 @@ class _TechcardState extends State<Techcard> {
             SizedBox(
               height: 10,
             ),
-            InkWell(
-              onTap: () => startup(),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 10,
-                    width: 10,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.yellow),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.yellow),
+                ),
+                Text(
+                  " Assingned Programs    ",
+                  style: TextStyle(
+                    fontFamily: "Nunito",
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff273746),
                   ),
-                  Text(
-                    " Assingned Programs    ",
-                    style: TextStyle(
-                      fontFamily: "Nunito",
-                      fontSize: 13,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff273746),
-                    ),
+                ),
+                Text(
+                  "$a",
+                  style: TextStyle(
+                    fontFamily: "Nunito",
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff273746),
                   ),
-                  Text(
-                    "$a",
-                    style: TextStyle(
-                      fontFamily: "Nunito",
-                      fontSize: 13,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff273746),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
             SizedBox(
               height: 10,
