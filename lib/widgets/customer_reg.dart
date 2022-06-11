6293f28c445498b15ecb888fe2765e20332ab168
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-06-11 18:36:25 +0530

    ui upto insta date

--- lib/widgets/customer_reg.dart
+++ lib/widgets/customer_reg.dart
@@ -12,7 +12,7 @@ class _CustomerregState extends State<Customerreg> {
   final _formkey = GlobalKey<FormState>();
 
   TextEditingController namecontroller = TextEditingController();
-  TextEditingController addresscontroller = TextEditingController();
+  TextEditingController addresscontroller = TextEditingController();  
   TextEditingController phncontroller = TextEditingController();
   TextEditingController prospeccontroller = TextEditingController();
   TextEditingController instadate = TextEditingController();
@@ -20,26 +20,26 @@ class _CustomerregState extends State<Customerreg> {
   @override
   Widget build(BuildContext context) {
     Size s = MediaQuery.of(context).size;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.only(left: 30, top: 30),
-          child: Text(
-            "Customer Registraion",
-            style: TextStyle(
-              fontFamily: "Montserrat",
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+    return Form(
+      key: _formkey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.only(left: 30, top: 30),
+            child: Text(
+              "Customer Registraion",
+              style: TextStyle(
+                fontFamily: "Montserrat",
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-        ),
-        const SizedBox(
-          height: 40,
-        ),
-        Form(
-          key: _formkey,
-          child: Row(
+          const SizedBox(
+            height: 40,
+          ),
+          Row(
             children: [
               Flexible(
                 flex: 1,
@@ -122,8 +122,92 @@ class _CustomerregState extends State<Customerreg> {
               )
             ],
           ),
-        )
-      ],
+          const SizedBox(height: 20,),
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: s.width * 0.05),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Product Specification",
+                        style: TextStyle(
+                            fontFamily: "Nunito",
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextFormField(
+                        autofocus: false,
+                        controller: prospeccontroller,
+                        textInputAction: TextInputAction.next,
+                        onSaved: (value) {
+                          prospeccontroller.text = value!;
+                        },
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return ("Please enter the Product Specification");
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: s.width * 0.04),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Installation Date",
+                        style: TextStyle(
+                            fontFamily: "Nunito",
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextFormField(
+                        autofocus: false,
+                        controller: addresscontroller,
+                        textInputAction: TextInputAction.next,
+                        onSaved: (value) {
+                          addresscontroller.text = value!;
+                        },
+                        validator: (value){
+                          if(value!.isEmpty){
+                            return ("Please enter the Installation Date");
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8))),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
