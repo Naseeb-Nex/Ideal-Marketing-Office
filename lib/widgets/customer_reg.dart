907ef7f9bf6f796cb1eac645632f0adf03f35d56
Naseeb-Nex <muhammednaseeb02@gmail.com>
2022-06-12 08:41:10 +0530

    new ui update

--- lib/widgets/customer_reg.dart
+++ lib/widgets/customer_reg.dart
@@ -12,10 +12,11 @@ class _CustomerregState extends State<Customerreg> {
   final _formkey = GlobalKey<FormState>();
 
   TextEditingController namecontroller = TextEditingController();
-  TextEditingController addresscontroller = TextEditingController();  
-  TextEditingController phncontroller = TextEditingController();
+  TextEditingController addresscontroller = TextEditingController();
   TextEditingController prospeccontroller = TextEditingController();
-  TextEditingController instadate = TextEditingController();
+  TextEditingController instadatecontroller = TextEditingController();
+  TextEditingController phn1controller = TextEditingController();
+  TextEditingController phn2controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,7 @@ class _CustomerregState extends State<Customerreg> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           const Padding(
-            padding: EdgeInsets.only(left: 30, top: 30),
+            padding: EdgeInsets.only(left: 30, top: 40),
             child: Text(
               "Customer Registraion",
               style: TextStyle(
@@ -66,8 +67,8 @@ class _CustomerregState extends State<Customerreg> {
                         onSaved: (value) {
                           namecontroller.text = value!;
                         },
-                        validator: (value){
-                          if(value!.isEmpty){
+                        validator: (value) {
+                          if (value!.isEmpty) {
                             return ("Please enter the Name");
                           }
                           return null;
@@ -102,12 +103,11 @@ class _CustomerregState extends State<Customerreg> {
                       TextFormField(
                         autofocus: false,
                         controller: addresscontroller,
-                        textInputAction: TextInputAction.next,
                         onSaved: (value) {
                           addresscontroller.text = value!;
                         },
-                        validator: (value){
-                          if(value!.isEmpty){
+                        validator: (value) {
+                          if (value!.isEmpty) {
                             return ("Please enter the Address");
                           }
                           return null;
@@ -116,13 +116,15 @@ class _CustomerregState extends State<Customerreg> {
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(8))),
                       ),
-                    ],
+                    ],  
                   ),
                 ),
               )
             ],
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           Row(
             children: [
               Flexible(
@@ -150,8 +152,8 @@ class _CustomerregState extends State<Customerreg> {
                         onSaved: (value) {
                           prospeccontroller.text = value!;
                         },
-                        validator: (value){
-                          if(value!.isEmpty){
+                        validator: (value) {
+                          if (value!.isEmpty) {
                             return ("Please enter the Product Specification");
                           }
                           return null;
@@ -185,13 +187,12 @@ class _CustomerregState extends State<Customerreg> {
                       ),
                       TextFormField(
                         autofocus: false,
-                        controller: addresscontroller,
-                        textInputAction: TextInputAction.next,
+                        controller: instadatecontroller,
                         onSaved: (value) {
-                          addresscontroller.text = value!;
+                          instadatecontroller.text = value!;
                         },
-                        validator: (value){
-                          if(value!.isEmpty){
+                        validator: (value) {
+                          if (value!.isEmpty) {
                             return ("Please enter the Installation Date");
                           }
                           return null;
@@ -205,6 +206,116 @@ class _CustomerregState extends State<Customerreg> {
                 ),
               ),
             ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: s.width * 0.05),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Phone Number 1",
+                        style: TextStyle(
+                            fontFamily: "Nunito",
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextFormField(
+                        autofocus: false,
+                        controller: phn1controller,
+                        textInputAction: TextInputAction.next,
+                        onSaved: (value) {
+                          phn1controller.text = value!;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return ("Please enter the Phone Number 1");
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 5.0, horizontal: s.width * 0.04),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Phone Number 2",
+                        style: TextStyle(
+                            fontFamily: "Nunito",
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextFormField(
+                        autofocus: false,
+                        controller: phn2controller,
+                        onSaved: (value) {
+                          phn2controller.text = value!;
+                        },
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8))),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Container(
+              width: s.width * 0.2,
+              padding: const EdgeInsets.symmetric(
+                vertical: 15,
+              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: bluebg,
+                  boxShadow: [
+                    BoxShadow(
+                        spreadRadius: 3,
+                        blurRadius: 5,
+                        color: Colors.black.withOpacity(0.2),
+                        offset: const Offset(0, 5))
+                  ]),
+              child: const Center(
+                child: Text(
+                  "Submit",
+                  style: TextStyle(
+                      fontFamily: "Nunito",
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: white),
+                ),
+              ),
+            ),
           )
         ],
       ),
