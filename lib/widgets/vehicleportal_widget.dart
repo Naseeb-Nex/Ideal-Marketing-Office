d18ae38ae13d18ce1740d651cbb5dc922241d0f7
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-11 12:42:29 +0530

    Feat: Vehicle portal Header added

--- lib/widgets/vehicleportal_widget.dart
+++ lib/widgets/vehicleportal_widget.dart
@@ -16,7 +16,8 @@ class VehiclePortalWidget extends StatefulWidget {
   String? username;
   String? techname;
 
-  VehiclePortalWidget({Key? key, this.username, this.techname}) : super(key: key);
+  VehiclePortalWidget({Key? key, this.username, this.techname})
+      : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
@@ -40,121 +41,37 @@ class _VehiclePortalWidgetState extends State<VehiclePortalWidget> {
               child: SingleChildScrollView(
                 scrollDirection: Axis.vertical,
                 child: Padding(
-                  padding: EdgeInsets.only(top: s.height * 0.02 ),
-                  child: Row(
+                  padding: EdgeInsets.only(top: s.height * 0.02),
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Flexible(
-                        fit: FlexFit.tight,
-                        flex: 5,
-                        child: Container(
-                          padding: EdgeInsets.all(s.width * 0.01),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(25),
-                            color: white,
-                            boxShadow: [
-                              BoxShadow(
-                                spreadRadius: 2,
-                                blurRadius: 4,
-                                color: black.withOpacity(.05),
-                                offset: const Offset(1, 2),
-                              ),
-                            ],
-                          ),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start, 
-                            children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween, 
-                              children: const [
-                                Text(
-                                  "Vehicle Garage",
-                                  style: TextStyle(
-                                    fontFamily: "Montserrat",
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 17,
-                                  ),
-                                ),
-                                Icon(Iconsax.more, color: black),
-                              ],
-                            ),
-                            const Divider(color: Color(0XFFadb5bd),),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            StreamBuilder<QuerySnapshot>(
-                                stream: fb.collection("Garage").snapshots(),
-                                builder: (BuildContext context,
-                                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                                  if (snapshot.hasError) {}
-                                  if (snapshot.connectionState ==
-                                      ConnectionState.waiting) {
-                                    return Center(
-                                      child: SizedBox(
-                                        width: s.width * 0.25,
-                                        height: s.width * 0.25,
-                                        child: const LoadingIndicator(
-                                          indicatorType:
-                                              Indicator.ballClipRotateMultiple,
-                                          colors: [bluebg],
-                                        ),
-                                      ),
-                                    );
-                                  }
-
-                                  final List vehicle = [];
-                                  snapshot.data!.docs
-                                      .map((DocumentSnapshot document) {
-                                    Map a =
-                                        document.data() as Map<String, dynamic>;
-                                    vehicle.add(a);
-                                    // a['uid'] = document.id;
-                                  }).toList();
-                                  return Column(
-                                    children: [
-                                      Container(
-                                          child: vehicle.isEmpty
-                                              ? Padding(
-                                                  padding: EdgeInsets.symmetric(
-                                                      horizontal:
-                                                          s.width * 0.01),
-                                                  child: Image.asset(
-                                                      "assets/icons/no_vehicles.jpg"),
-                                                )
-                                              : null),
-                                      for (var i = 0;
-                                          i < vehicle.length;
-                                          i++) ...[
-                                        Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              vertical: 5.0),
-                                          child: VechicleInfoCard(
-                                            name: vehicle[i]['name'],
-                                            desc: vehicle[i]['description'],
-                                            type: vehicle[i]['type'],
-                                            status: vehicle[i]['status'],
-                                            techname: vehicle[i]['techname'],
-                                            username: vehicle[i]['username'],
-                                            vdocname: vehicle[i]['docname'],
-                                            statusdesc: vehicle[i]
-                                                ['statusdesc'],
-                                            update: vehicle[i]['update'],
-                                            uptime: vehicle[i]['uptime'],
-                                          ),
-                                        )
-                                      ]
-                                    ],
-                                  );
-                                }),
-                          ]),
-                        ),
+                      const Text(
+                        "Vehicle Portal",
+                        style: TextStyle(
+                            fontFamily: "Montserrat",
+                            fontSize: 23,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0XFF333333)),
                       ),
-                      Flexible(
-                          flex: 2,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 10),
+                      const SizedBox(height: 3,),
+                       Text(
+                        "Its a Grage! For managing vehicles",
+                        style: TextStyle(
+                            fontFamily: "Montserrat",
+                            fontSize: 13,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.blueGrey.shade500,),
+                      ),
+                      // SizedBox(height: s.height * 0.04,),
+                      const Divider(),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Flexible(
+                            fit: FlexFit.tight,
+                            flex: 5,
                             child: Container(
-                              padding: EdgeInsets.all(s.width * 0.02),
+                              padding: EdgeInsets.all(s.width * 0.01),
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(25),
                                 color: white,
@@ -168,27 +85,152 @@ class _VehiclePortalWidgetState extends State<VehiclePortalWidget> {
                                 ],
                               ),
                               child: Column(
-                                children: [
-                                  Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start, 
-                              children: const [
-                                Text(
-                                  "Recent Activity",
-                                  style: TextStyle(
-                                    fontFamily: "Montserrat",
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 17,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: const [
+                                        Text(
+                                          "Vehicle Garage",
+                                          style: TextStyle(
+                                            fontFamily: "Montserrat",
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 17,
+                                          ),
+                                        ),
+                                        Icon(Iconsax.more, color: black),
+                                      ],
+                                    ),
+                                    const Divider(
+                                      color: Color(0XFFadb5bd),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    StreamBuilder<QuerySnapshot>(
+                                        stream:
+                                            fb.collection("Garage").snapshots(),
+                                        builder: (BuildContext context,
+                                            AsyncSnapshot<QuerySnapshot>
+                                                snapshot) {
+                                          if (snapshot.hasError) {}
+                                          if (snapshot.connectionState ==
+                                              ConnectionState.waiting) {
+                                            return Center(
+                                              child: SizedBox(
+                                                width: s.width * 0.1,
+                                                height: s.width * 0.1,
+                                                child: const LoadingIndicator(
+                                                  indicatorType: Indicator
+                                                      .ballClipRotateMultiple,
+                                                  colors: [bluebg],
+                                                ),
+                                              ),
+                                            );
+                                          }
+
+                                          final List vehicle = [];
+                                          snapshot.data!.docs
+                                              .map((DocumentSnapshot document) {
+                                            Map a = document.data()
+                                                as Map<String, dynamic>;
+                                            vehicle.add(a);
+                                            // a['uid'] = document.id;
+                                          }).toList();
+                                          return Column(
+                                            children: [
+                                              Container(
+                                                  child: vehicle.isEmpty
+                                                      ? Padding(
+                                                          padding: EdgeInsets
+                                                              .symmetric(
+                                                                  horizontal:
+                                                                      s.width *
+                                                                          0.01),
+                                                          child: Image.asset(
+                                                              "assets/icons/no_vehicles.jpg"),
+                                                        )
+                                                      : null),
+                                              for (var i = 0;
+                                                  i < vehicle.length;
+                                                  i++) ...[
+                                                Padding(
+                                                  padding: const EdgeInsets
+                                                      .symmetric(vertical: 5.0),
+                                                  child: VechicleInfoCard(
+                                                    name: vehicle[i]['name'],
+                                                    desc: vehicle[i]
+                                                        ['description'],
+                                                    type: vehicle[i]['type'],
+                                                    status: vehicle[i]
+                                                        ['status'],
+                                                    techname: vehicle[i]
+                                                        ['techname'],
+                                                    username: vehicle[i]
+                                                        ['username'],
+                                                    vdocname: vehicle[i]
+                                                        ['docname'],
+                                                    statusdesc: vehicle[i]
+                                                        ['statusdesc'],
+                                                    update: vehicle[i]
+                                                        ['update'],
+                                                    uptime: vehicle[i]
+                                                        ['uptime'],
+                                                  ),
+                                                )
+                                              ]
+                                            ],
+                                          );
+                                        }),
+                                  ]),
+                            ),
+                          ),
+                          Flexible(
+                              flex: 2,
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: Container(
+                                  padding: EdgeInsets.all(s.width * 0.02),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(25),
+                                    color: white,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        spreadRadius: 2,
+                                        blurRadius: 4,
+                                        color: black.withOpacity(.05),
+                                        offset: const Offset(1, 2),
+                                      ),
+                                    ],
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: const [
+                                          Text(
+                                            "Recent Activity",
+                                            style: TextStyle(
+                                              fontFamily: "Montserrat",
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 17,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      const Divider(
+                                        color: Color(0XFFadb5bd),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                              ],
-                            ),
-                            const Divider(color: Color(0XFFadb5bd),),
-                            
-                                ],
-                              ),
-                            ),
-                          )),
+                              )),
+                        ],
+                      ),
                     ],
                   ),
                 ),
