138b7c22ee5434b039d69515a6b8014ac2cf3321
Naseeb-Nex <muhammednaseeb02@gmail.com>
2023-02-13 21:19:01 +0530

    Feat: New animation added Vehicle button added

--- lib/widgets/asign_pgm_widget.dart
+++ lib/widgets/asign_pgm_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:test2/componets/assign_pgm_card.dart';
 import 'package:test2/constants/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -29,6 +30,8 @@ class _AssignpgmwidgetState extends State<Assignpgmwidget> {
 
   final Stream<QuerySnapshot> programstream =
       FirebaseFirestore.instance.collection('Programs').snapshots();
+  double animSize = 0;
+  bool vButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -36,53 +39,202 @@ class _AssignpgmwidgetState extends State<Assignpgmwidget> {
     const styleHint = TextStyle(color: Colors.black54);
     final style = controller.text.isEmpty ? styleHint : styleActive;
 
+    // Size
+    Size s = MediaQuery.of(context).size;
+
     return Column(
       children: [
-        const SizedBox(
-          height: 20,
-        ),
-        Container(
-          width: 700,
-          height: 40,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: primarybg,
-            boxShadow: [
-              BoxShadow(
-                offset: const Offset(0, 5),
-                blurRadius: 5,
-                color: secondbg.withOpacity(0.20),
+        Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Container(
+                  width: s.width * 0.5,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: primarybg,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 5),
+                        blurRadius: 5,
+                        color: secondbg.withOpacity(0.20),
+                      ),
+                    ],
+                  ),
+                  child: Container(
+                    height: double.infinity,
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    child: TextField(
+                      controller: controller,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.search, color: style.color),
+                        suffixIcon: controller.text.isNotEmpty
+                            ? GestureDetector(
+                                child: Icon(Icons.close, color: style.color),
+                                onTap: () {
+                                  controller.clear();
+                                  searchpgm('');
+                                  FocusScope.of(context)
+                                      .requestFocus(FocusNode());
+                                },
+                              )
+                            : null,
+                        hintText: "Search here",
+                        hintStyle: style,
+                        border: InputBorder.none,
+                      ),
+                      style: style,
+                      onChanged: searchpgm,
+                    ),
+                  ),
+                ),
               ),
-            ],
-          ),
-          child: Container(
-            height: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-            child: TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                icon: Icon(Icons.search, color: style.color),
-                suffixIcon: controller.text.isNotEmpty
-                    ? GestureDetector(
-                        child: Icon(Icons.close, color: style.color),
-                        onTap: () {
-                          controller.clear();
-                          searchpgm('');
-                          FocusScope.of(context).requestFocus(FocusNode());
-                        },
-                      )
-                    : null,
-                hintText: "Search here",
-                hintStyle: style,
-                border: InputBorder.none,
-              ),
-              style: style,
-              onChanged: searchpgm,
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 5),
+                            child: AnimatedContainer(
+                              width: animSize,
+                              height: animSize,
+                              duration: const Duration(milliseconds: 1400),
+                              curve: Curves.fastOutSlowIn,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: bluebg,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      spreadRadius: 2,
+                                      blurRadius: 7,
+                                      color: bluebg.withOpacity(0.25),
+                                      offset: const Offset(0, 4),
+                                    )
+                                  ]),
+                              child: const Icon(
+                                Iconsax.tag_cross,
+                                color: white,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 5),
+                            child: AnimatedContainer(
+                              width: animSize,
+                              height: animSize,
+                              duration: const Duration(milliseconds: 1200),
+                              curve: Curves.fastOutSlowIn,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: bluebg,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      spreadRadius: 2,
+                                      blurRadius: 7,
+                                      color: bluebg.withOpacity(0.25),
+                                      offset: const Offset(0, 4),
+                                    )
+                                  ]),
+                              child: const Icon(
+                                Icons.directions_bike_rounded,
+                                color: white,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 5),
+                            child: AnimatedContainer(
+                              width: animSize,
+                              height: animSize,
+                              duration: const Duration(milliseconds: 1000),
+                              curve: Curves.fastOutSlowIn,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: bluebg,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      spreadRadius: 2,
+                                      blurRadius: 7,
+                                      color: bluebg.withOpacity(0.25),
+                                      offset: const Offset(0, 4),
+                                    )
+                                  ]),
+                              child: const Icon(
+                                Iconsax.add,
+                                color: white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            vButton = !vButton;
+                          });
+                          if (animSize == 35) {
+                            setState(() {
+                              animSize = 0;
+                            });
+                          } else {
+                            setState(() {
+                              animSize = 35;
+                            });
+                          }
+                        },
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: vButton ? white : bluebg,
+                              boxShadow: [
+                                BoxShadow(
+                                  spreadRadius: 2,
+                                  blurRadius: 7,
+                                  color: bluebg.withOpacity(0.25),
+                                  offset: const Offset(0, 4),
+                                )
+                              ]),
+                          child: vButton
+                              ? const Center(
+                                  child: Text(
+                                    "X",
+                                    style: TextStyle(
+                                      fontFamily: "Montserrat",
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 18,
+                                      color: bluebg,
+                                    ),
+                                  ),
+                                )
+                              : const Icon(
+                                  Icons.directions_bike_rounded,
+                                  color: white,
+                                ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
         ),
         const SizedBox(
-          height: 30,
+          height: 10,
         ),
         Expanded(
           child: Padding(
